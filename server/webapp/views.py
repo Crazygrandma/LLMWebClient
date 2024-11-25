a0505ce0a4a0a055fe7548dd9dccaf234f69437d
Henry <henry5@uni-bremen.de>
2024-11-25 01:40:53 +0100

    Add gpt4all class

--- server/webapp/views.py
+++ server/webapp/views.py
@@ -5,3 +5,53 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world.")
+
+
+
+# from gpt4all import GPT4All
+
+# class GPTManager:
+
+#     def __init__(self,model,system_prompt='',device='gpu'):
+#         # self.sytem_prompt = system_prompt
+#         self.model = f'c:/Users/henry/AppData/Local/nomic.ai/GPT4All/{model}'
+#         self.gpt = GPT4All(model_name=self.model,device=device, allow_download=False)
+
+#     def setDevice(self,device="gpu"):
+#         self.device = device
+
+#     def getResponse(self,prompt):
+#         response = self.gpt.generate(
+#             prompt=prompt,
+#             streaming=True
+#         )
+#         return response
+
+
+# def main():
+#     system_prompt = ''''''
+#     # model = "mistral-7b-instruct-v0.1.Q4_0.gguf"
+#     # model = "em_german_mistral_v01.Q4_0.gguf"
+#     model = "em_german_leo_mistral.Q4_0.gguf"
+#     # model = "em_german_7b_v01.Q4_K_M.gguf"
+#     # model = "mistral-7b-openorca.Q4_0.gguf"
+#     # model = "Llama-3.2-1B-Instruct-Q4_0.gguf"
+#     # model = 'c:/Users/henry/AppData/Local/nomic.ai/GPT4All/mistral-7b-openorca.Q4_0.gguf'
+    
+    
+    
+#     mygpt = GPTManager(model)
+#     clientinput = ""
+#     tokenGenerator = mygpt.gpt.generate(clientinput);
+    
+#     # print(tokenGenerator
+#     # next(tokenGenerator)
+#     reponseText = ""
+#     for value in tokenGenerator:
+#         # print(value)
+#         reponseText += value
+        
+#         if len(reponseText) >= 50:
+            
+#             print(reponseText)
+#             reponseText = ""
